daf0b967273c81bec2e8d3d53a32b58600e5062b
Brae Webb <email@braewebb.com>
2021-08-31 11:45:45 +1000

    Bibtex references in markdown

--- bin/build.sh
+++ bin/build.sh
@@ -3,5 +3,27 @@
 REPO_ROOT=$(git rev-parse --show-toplevel)
 
 mkdir -p "${REPO_ROOT}/dist"
+mkdir -p "${REPO_ROOT}/dist/handouts"
+mkdir -p "${REPO_ROOT}/dist/slides"
+
 
 pandoc "${REPO_ROOT}/cases/"*.md -o "${REPO_ROOT}/dist/SoftwareArchitectureCases.pdf"
+
+CONTENT="architectures.layered,LayeredArchitecture qualities,QualityAttributes"
+
+for i in ${CONTENT}; do IFS=","; set -- $i;
+    # python3 -m presentations $1
+    # cp "${REPO_ROOT}/workdir/$1/slides.pdf" "${REPO_ROOT}/dist/slides/$2.pdf"
+    # cp -r "${REPO_ROOT}/workdir/images" "${REPO_ROOT}"
+
+    notes="${1//.//}.md"
+    pandoc "${REPO_ROOT}/notes/${notes}" \
+        -o "${REPO_ROOT}/dist/handouts/$2.pdf" \
+        --bibliography ${REPO_ROOT}/references/books.bib \
+        --bibliography ${REPO_ROOT}/references/articles.bib \
+        --csl ${REPO_ROOT}/references/style.csl
+done
+# pandoc "${REPO_ROOT}/notes/architectures/layered.md" -o "${REPO_ROOT}/dist/handouts/LayeredArchitecture.pdf"
+
+# python3 -m presentations layered
+# cp "${REPO_ROOT}/workdir/layered/slides.pdf" "${REPO_ROOT}/workdir/slides/LayeredArchitecture.pdf"
