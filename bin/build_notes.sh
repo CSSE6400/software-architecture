8cf2aa213a06e1c83a337aa700c48f796508d59f
Brae Webb <email@braewebb.com>
2021-12-27 15:58:09 +1000

    Trigger note building on push

--- bin/build_notes.sh
+++ bin/build_notes.sh
@@ -5,8 +5,6 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 mkdir -p "${REPO_ROOT}/dist"
 mkdir -p "${REPO_ROOT}/dist/handouts"
 
-pandoc "${REPO_ROOT}/cases/"*.md -o "${REPO_ROOT}/dist/SoftwareArchitectureCases.pdf"
-
 . "${REPO_ROOT}/bin/_content.sh"
 
 for i in ${CONTENT}; do IFS=","; set -- $i;
