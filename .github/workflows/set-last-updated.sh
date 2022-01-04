030378e8155adb0707e17a2cb51a69f9ba1419f7
Brae Webb <email@braewebb.com>
2022-01-04 16:59:16 +1000

    Script to ensure last modified dates are accurate

---
+++ .github/workflows/set-last-updated.sh
@@ -0,0 +1,13 @@
+# thanks Dietrich
+# https://stackoverflow.com/a/2179876/13430616
+
+IFS="
+"
+for FILE in $(git ls-files)
+do
+    TIME=$(git log --pretty=format:%cd -n 1 --date=iso --date-order -- "$FILE")
+
+    TIME=$(date --date="$TIME" +%Y%m%d%H%M.%S)
+
+    touch -m -t "$TIME" "$FILE"
+done
