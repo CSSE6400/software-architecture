a62192ebaffb52cf74b5a7fb007d7a5a05976bf2
Brae Webb <email@braewebb.com>
2022-01-06 13:06:04 +1000

    Swap to latex slide building

--- bin/build_slides.sh
+++ bin/build_slides.sh
@@ -5,10 +5,12 @@ REPO_ROOT=$(git rev-parse --show-toplevel)
 mkdir -p "${REPO_ROOT}/dist"
 mkdir -p "${REPO_ROOT}/dist/slides"
 
-. "${REPO_ROOT}/bin/_content.sh"
-
-for i in ${CONTENT}; do IFS=","; set -- $i;
-    python3 -m presentations $1
-    cp "${REPO_ROOT}/workdir/$1/slides.pdf" "${REPO_ROOT}/dist/slides/$2.pdf"
-    cp -r "${REPO_ROOT}/workdir/images" "${REPO_ROOT}"
+for folder in ${REPO_ROOT}/slides/*; do
+    echo $folder
+    if [[ -f "${folder}/latexmkrc" ]]; then
+        pushd $folder;
+        latexmk
+        cp ./out/main.pdf "${REPO_ROOT}/dist/slides/$(basename $folder).pdf"
+        popd
+    fi
 done
