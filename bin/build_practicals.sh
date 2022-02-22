99144775bff90bdd7bf5c652aa2ae72b4c2b7f01
Evan Hughes <ehugh1@gmail.com>
2022-02-23 02:58:51 +1000

    Adding some dynamic plantuml diagram generation

--- bin/build_practicals.sh
+++ bin/build_practicals.sh
@@ -7,6 +7,12 @@ mkdir -p "${REPO_ROOT}/dist/practicals"
 
 for folder in ${REPO_ROOT}/practicals/*; do
     echo $folder
+    if [[ -d "${folder}/images" ]]; then
+        for puml in ${folder}/images/*.puml; do
+            [ -f "$puml" ] || break
+            java -jar "${REPO_ROOT}/bin/plantuml.jar" -png $puml
+        done
+    fi
     if [[ -f "${folder}/latexmkrc" ]]; then
         pushd $folder;
         latexmk -halt-on-error
