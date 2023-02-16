10581b8cc6b7ebaa0cc9602a4527ead970ab628f
Brae Webb <email@braewebb.com>
2023-02-16 14:22:08 +1000

    Extend build script to also build HTML

--- bin/build_notes.sh
+++ bin/build_notes.sh
@@ -9,12 +9,22 @@ for folder in ${REPO_ROOT}/notes/*; do
     echo $folder
     if [[ -f "${folder}/latexmkrc" ]]; then
         pushd $folder;
+        # Build PDFs
         latexmk -halt-on-error
         if [ $? -ne 0 ]; then
             echo "error: $(basename $folder) notes failed to build"
             exit 1
         fi
         cp ./out/main.pdf "${REPO_ROOT}/dist/handouts/$(basename $folder).pdf"
+
+        # Build HTML
+        latexmk -pdf- -dvi -outdir= -halt-on-error
+        if [ $? -ne 0 ]; then
+            echo "error: $(basename $folder) notes failed to build"
+            exit 1
+        fi
+        mkdir -p "${REPO_ROOT}/dist/handouts/$(basename $folder)"
+        cp ./main.html "${REPO_ROOT}/dist/handouts/$(basename $folder)/index.html"
         popd
     fi
 done
