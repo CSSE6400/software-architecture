7e5b2a5e4a5ecd7d0f654dae43befffca9dcac69
Richard Thomas <richard.thomas@uq.edu.au>
2023-04-28 17:07:20 +1000

    Added persistence requirement to spec and changed POST success responses to 201.

--- assessment/cloud/build-docs.bat
+++ assessment/cloud/build-docs.bat
@@ -1,4 +1,4 @@
 REM Docker on Windows can only map a local hard drive, not a network mapped hard drive.
-REM Copy command below and run from command line. Docker doesn't copy with Windows path parameters.
+REM Copy command below and run in PowerShell. Docker doesn't cope with Windows path parameters.
 
 docker run -v ${pwd}:/workspace --rm openapitools/openapi-generator-cli:latest generate --generator-name html2 --output /workspace/docs --input-spec /workspace/spec.yml --template-dir /workspace/docs-template
