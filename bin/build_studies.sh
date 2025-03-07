206709d720eb7cda8360425eebc664b8f840600e
Richard Thomas <richard.thomas@uq.edu.au>
2025-03-07 18:22:53 +1000

    Update build_studies.sh to publish case study write ups.

--- bin/build_studies.sh
+++ bin/build_studies.sh
@@ -13,15 +13,19 @@ for folder in ${REPO_ROOT}/studies/*; do
         # Build PDFs
         latexmk -halt-on-error
         if [ $? -ne 0 ]; then
-            echo "error: $(basename $folder) tutorial failed to build"
+            echo "error: $(basename $folder) case study failed to build as PDF"
             exit 1
         fi
         cp ./out/main.pdf "${DIST_DIR}/$(basename $folder).pdf"
+		# If the writeup of the sample solution exists, copy it as well.
+		if [ -f ./out/writeup.pdf ]; then
+			cp ./out/writeup.pdf "${DIST_DIR}/$(basename $folder)-writeup.pdf"
+		fi
 
         # Build HTML
         latexmk -pdf- -dvi -outdir= -halt-on-error
         if [ $? -ne 0 ]; then
-            echo "error: $(basename $folder) notes failed to build"
+            echo "error: $(basename $folder) case study failed to build as HTML"
         #    exit 1
         else
             HTML_DIR="${DIST_DIR}/$(basename $folder)"
