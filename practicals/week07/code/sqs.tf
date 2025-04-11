a45ace61b8a20d6a7cd113e29e0ecdb701b2b04c
Richard Thomas <richard.thomas@uq.edu.au>
2025-04-11 22:46:45 +1000

    More detail about deploying SQS and accessing it in prac 7.

--- practicals/week07/code/sqs.tf
+++ practicals/week07/code/sqs.tf
@@ -2,30 +2,22 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 5.0"
     }
   }
 }
 
 provider "aws" {
   region = "us-east-1"
-  shared_credentials_file = "./credentials"
+  shared_credentials_files = ["./credentials"]
 }
 
-resource "aws_sqs_queue" "our_first_mailbox" {
-  name                        = "csse6400_prac"
+resource "aws_sqs_queue" "ical_queue" {
+    name = "ical"
 }
 
-resource "aws_sqs_queue" "our_first_fifo" {
-  name                        = "csse6400_prac.fifo"
-  fifo_queue                  = true
-  content_based_deduplication = true
-}
-
-output "mailbox" {
-  value = aws_sqs_queue.our_first_mailbox.arn
-}
-
-output "fifo" {
-  value = aws_sqs_queue.our_first_fifo.arn
-}
+# Output the URL of the ical SQS queue.
+# Not needed for the practical, as Celery manages the connection to SQS using AWS credentials.
+#output "sqs_url_resource_attribute" {
+#  value = aws_sqs_queue.ical_queue.id
+#}
